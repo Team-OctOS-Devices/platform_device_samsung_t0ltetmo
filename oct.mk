19cd0d414619a4da31e51ee93eac8ea0a52f7820
hedwig34 <hedwig34@gmail.com>
2014-10-23 14:10:04 -0400

    t0ltetmo: OctOs-Fy t0ltetmo

--- oct.mk
+++ oct.mk
@@ -1,6 +1,3 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := t0ltetmo
 
@@ -8,15 +5,15 @@ PRODUCT_RELEASE_NAME := t0ltetmo
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common oct stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0ltetmo
-PRODUCT_NAME := cm_t0ltetmo
+PRODUCT_NAME := oct_t0ltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-T889
 PRODUCT_MANUFACTURER := samsung
